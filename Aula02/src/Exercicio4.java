8300a0b03adf348a37896257e1d7ae42e3a1e6f8
John Chappell <john.claude.cameron@gmail.com>
2024-03-01 10:58:27 -0300

    Update Exercicio4.java

--- Aula02/src/Exercicio4.java
+++ Aula02/src/Exercicio4.java
@@ -7,21 +7,15 @@ public class Exercicio4 {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Digite o primeiro número: ");
-        int num1 = input.nextInt();
+        float num1 = input.nextInt();
         
         System.out.print("Digite o segundo número: ");
-        int num2 = input.nextInt();
+        float num2 = input.nextInt();
 
-        int soma = num1 + num2;
-        int sub = num1 - num2;
-        int mult = num1 * num2;
-        int div = num1 / num2;
-
-        System.out.println("Soma dos números: " + soma);
-        System.out.println("Subtração dos números: " + sub);
-        System.out.println("Multiplicação dos números: " + mult);
-        System.out.println("Divisão dos números: " + div);
+        System.out.println("Soma dos números: " + (num1 + num2));
+        System.out.println("Subtração dos números: " + (num1 - num2));
+        System.out.println("Multiplicação dos números: " + (num1 * num2));
+        System.out.println("Divisão dos números: " + (num1 / num2));
 
     }
-
 }
