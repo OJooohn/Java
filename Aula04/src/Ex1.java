4087ae9a041a8622dea544c78c55f5da265a3f08
John Chappell <john.claude.cameron@gmail.com>
2024-03-10 22:27:58 -0300

    Upload

---
+++ Aula04/src/Ex1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import javax.print.attribute.standard.Media;
+
+public class Ex1 {
+
+  static float Media(float nota1, float nota2, float nota3){
+
+    return (nota1 + nota2 + nota3) / 3;
+
+  }
+  
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+
+    float nota1, nota2, nota3;
+
+    System.out.print("Digite a nota1: ");
+    nota1 = Float.parseFloat(input.nextLine());
+
+    System.out.print("Digite a nota2: ");
+    nota2 = Float.parseFloat(input.nextLine());
+
+    System.out.print("Digite a nota3: ");
+    nota3 = Float.parseFloat(input.nextLine());
+
+    System.out.println("Média aritmética final do aluno = " + Media(nota1, nota2, nota3));
+
+  }
+
+}
---
+++ Aula04/src/Ex4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex4 {
+
+  static float conversao(float cotacao, float dolares){
+
+    return (cotacao / dolares);
+
+  }
+  
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+
+    float cotacao, dolares;
+
+    System.out.print("Digite a quantidade de dolares: ");
+    dolares = Float.parseFloat(input.nextLine());
+
+    System.out.print("Digite o valor da cotação: ");
+    cotacao = Float.parseFloat(input.nextLine());
+
+    System.out.println("Total = R$ " + conversao(cotacao, dolares));
+
+  }
+
+}
