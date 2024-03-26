3b1bdbdfef0a8b44c0c4e3b0c73d3253db75f5f0
John Chappell <john.claude.cameron@gmail.com>
2024-03-26 18:00:53 -0300

    Commit

---
+++ Lista 01/Ex8/src/Pedido.java
@@ -0,0 +1,9 @@
+public class Pedido {
+  
+  private String tipo;
+  private String tamanho;
+  private float preco;
+  private String endereco;
+  private boolean ativo;
+
+}
---
+++ Lista 01/Ex8/src/Pizza.java
@@ -0,0 +1,5 @@
+public class Pizza {
+  
+  private String nome;
+
+}
