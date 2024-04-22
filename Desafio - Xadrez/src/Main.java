c763acdbba02f24e9764c969959a4be11fdbddfa
John Chappell <john.claude.cameron@gmail.com>
2024-04-22 00:14:05 -0300

    Update

--- Desafio - Xadrez/src/Main.java
+++ Desafio - Xadrez/src/Main.java
@@ -17,35 +17,9 @@ public class Main {
     // Reset
     public static final String RESET = "\033[0m";  // Text Reset
 
-    // Regular Colors
-    public static final String BLACK = "\033[0;30m";   // BLACK
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
-    public static final String YELLOW = "\u001B[93m";  // YELLOW
-    public static final String BLUE = "\u001B[94m";    // BLUE
-    public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m";   // WHITE
-
-    // Bold
-    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-    public static final String RED_BOLD = "\033[1;31m";    // RED
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
-
     // Background
-    public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
-    public static final String RED_BACKGROUND = "\033[41m";    // RED
+    public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK// RED
     public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
-    public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
-    public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
-    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
-    public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
-    public static final String WHITE_BACKGROUND = "\033[48m";  // WHITE
 
     public static void printBackground(int i, int j){
         if(i % 2 == 0){
@@ -85,10 +59,6 @@ public class Main {
                     
                     if (p.getposX() == j && p.getposY() == i) {
                         System.out.print("[ " + p.getIcone() +  " ]" + RESET);
-                        //printBackground(i, j);
-                        //System.out.print("[ " + RESET + p.getIcone());
-                        //printBackground(i, j);
-                        //System.out.print( " ]" + RESET);
                         encontrado = true;
                         break;
                     }
@@ -96,25 +66,17 @@ public class Main {
                 for (Inimigo I : inimigos) {
                     if (I.getposX() == j && I.getposY() == i) {
                         System.out.print("[ " + I.getIcone() +  " ]" + RESET);
-                        //printBackground(i, j);
-                        //System.out.print("[ " + RESET + I.getIcone());
-                        //printBackground(i, j);
-                        //System.out.print( " ]" + RESET);
                         encontrado = true;
                         break;
                     }
                 }
-                if (!encontrado){
-                    //System.out.print("[");
-                    //printBackground(i, j);
-                    //System.out.print("   "  + RESET);
-                    //System.out.print("]");
+                if (!encontrado)
                     System.out.print("[   ]" + RESET);
-                }
+
                 if(i == 7)
-                    System.out.printf("\t");
+                    System.out.print("\t");
             }
-            System.out.print("[ " + BLUE_BOLD + letra + RESET + " ]");
+            System.out.print("[ " + letra + RESET + " ]");
             System.out.println();
             if(j == 7)
                 System.out.println();
@@ -129,7 +91,7 @@ public class Main {
         System.out.println("--------------------------------------------------------------");
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
@@ -145,7 +107,7 @@ public class Main {
         personagens.add(new Personagem("♔", 7, 4));
 
         // Peões
-        for(i = 0; i < 8; i++){
+        for(i = 0; i < tabuleiro.getColunas(); i++){
             //personagens.add(new Personagem("♙" , 6, i));
             personagens.add(new Personagem("♙" , 6, i));
         }
@@ -170,7 +132,7 @@ public class Main {
         inimigos.add(new Inimigo("♚", 0, 4));
 
         // Peões
-        for(i = 0; i < 8; i++){
+        for(i = 0; i < tabuleiro.getColunas(); i++){
             inimigos.add(new Inimigo("♟", 1, i));
         }
 
@@ -198,28 +160,36 @@ public class Main {
             int pecas = rodada % 2;
             int linha, coluna;
             char caracterLinha, caracterColuna;
-            String pecaSelecionada = "", proximaPosicao = "";
+            String pecaSelecionada, proximaPosicao;
             String[] cortado;
 
             imprimirTabuleiro(personagens, inimigos);
-            if(personagens.get(0).getIcone() != "♔"){
+            if(!personagens.get(0).getIcone().equals("♔")){
                 System.out.println("- PEÇAS PRETAS VENCERAM!                                     -");
                 System.out.println("--------------------------------------------------------------");
-                menu = false;
                 break;
             }
-            if(inimigos.get(0).getIcone() != "♚"){
-                System.out.println("- PEÇAS BRANCAS VENCERAM!                                     -");
+            if(!inimigos.get(0).getIcone().equals("♚")){
+                System.out.println("- PEÇAS PRETAS VENCERAM!                                     -");
                 System.out.println("--------------------------------------------------------------");
-                menu = false;
                 break;
             }
+
             switch(pecas){
                 case 0:
                     System.out.println("- RODADA DAS PEÇAS BRANCAS                                   -");
                     System.out.println("--------------------------------------------------------------");
+                    System.out.println("- DIGITE 'SAIR' PARA FINALIZAR O JOGO                        -");
                     System.out.print("- SELECIONE A PEÇA (A1 - H8): ");
                     pecaSelecionada = input.nextLine();
+
+                    if(pecaSelecionada.equalsIgnoreCase("SAIR")){
+                        System.out.println("--------------------------------------------------------------");
+                        System.out.println("- JOGO FINALIZADO SEM VENCEDORES                             -");
+                        System.out.println("--------------------------------------------------------------");
+                        menu = false;
+                        break;
+                    }
                     
                     // Se o tamanho da string for diferente de 2
                     // !Character.isLetter(pecaSelecionada.charAt(0)) --> Se o primeiro caracter não é uma letra entra pro input inválido
@@ -264,7 +234,7 @@ public class Main {
                             System.out.print("- SELECIONE A PROXIMA POSIÇÃO DA PEÇA (A1 - H8): ");
                             proximaPosicao = input.nextLine();
 
-                            if (pecaSelecionada.length() != 2 || !Character.isLetter(pecaSelecionada.charAt(0)) || !Character.isDigit(pecaSelecionada.charAt(1))) {
+                            if (!Character.isLetter(pecaSelecionada.charAt(0)) || !Character.isDigit(pecaSelecionada.charAt(1))) {
                                 System.out.println("- Input inválido. Insira um input válido entre 'A1' e 'H8'.");
                             } else {
 
@@ -306,8 +276,17 @@ public class Main {
                 case 1:
                     System.out.println("- RODADA DAS PEÇAS PRETAS                                   -");
                     System.out.println("--------------------------------------------------------------");
+                    System.out.println("- DIGITE 'SAIR' PARA FINALIZAR O JOGO                        -");
                     System.out.print("- SELECIONE A PEÇA (A1 - H8): ");
                     pecaSelecionada = input.nextLine();
+
+                    if(pecaSelecionada.equalsIgnoreCase("SAIR")){
+                        System.out.println("--------------------------------------------------------------");
+                        System.out.println("- JOGO FINALIZADO SEM VENCEDORES                             -");
+                        System.out.println("--------------------------------------------------------------");
+                        menu = false;
+                        break;
+                    }
                     
                     // Se o tamanho da string for diferente de 2
                     // !Character.isLetter(pecaSelecionada.charAt(0)) --> Se o primeiro caracter não é uma letra entra pro input inválido
@@ -350,7 +329,7 @@ public class Main {
                             System.out.print("- SELECIONE A PROXIMA POSIÇÃO DA PEÇA (A1 - H8): ");
                             proximaPosicao = input.nextLine();
 
-                            if (pecaSelecionada.length() != 2 || !Character.isLetter(pecaSelecionada.charAt(0)) || !Character.isDigit(pecaSelecionada.charAt(1))) {
+                            if (!Character.isLetter(pecaSelecionada.charAt(0)) || !Character.isDigit(pecaSelecionada.charAt(1))) {
                                 System.out.println("- Input inválido. Insira um input válido entre 'A1' e 'H8'.");
                             } else {
 
